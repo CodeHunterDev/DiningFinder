c8acd2525287a43613d62744c4c43db9ebc7bcdf
elbehiry <m.elbehiry44@gmail.com>
2021-07-16 16:25:35 +0200

    🎉 Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-android")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    compileSdk = Versions.COMPILE_SDK
+    buildToolsVersion = "30.0.3"
+    defaultConfig {
+        applicationId = "com.elbehiry.diningfinder"
+        minSdk = Versions.MIN_SDK
+        targetSdk = Versions.TARGET_SDK
+        versionCode = Versions.versionCodeMobile
+        versionName = Versions.versionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables.useSupportLibrary = true
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments["dagger.hilt.disableModulesHaveInstallInCheck"] = "true"
+            }
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        getByName("debug") {
+            versionNameSuffix = "-debug"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    kotlinOptions {
+        val options = this
+        options.jvmTarget = "1.8"
+    }
+
+    packagingOptions {
+        resources.excludes += "META-INF/AL2.0"
+        resources.excludes += "META-INF/LGPL2.1"
+    }
+}
+
+dependencies {
+    api(platform(project(":depconstraints")))
+    kapt(platform(project(":depconstraints")))
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation(Libs.CORE_KTX)
+    // UI
+    implementation(Libs.ACTIVITY_KTX)
+    implementation(Libs.APPCOMPAT)
+    implementation(Libs.FRAGMENT_KTX)
+    implementation(Libs.CARDVIEW)
+    implementation(Libs.CONSTRAINT_LAYOUT)
+    implementation(Libs.DRAWER_LAYOUT)
+    implementation(Libs.MATERIAL)
+
+    // Architecture Components
+    testImplementation(Libs.ARCH_TESTING)
+    implementation(Libs.NAVIGATION_FRAGMENT_KTX)
+    implementation(Libs.NAVIGATION_UI_KTX)
+
+    // Dagger Hilt
+    implementation(Libs.HILT_ANDROID)
+    androidTestImplementation(Libs.HILT_TESTING)
+    kapt(Libs.HILT_COMPILER)
+    kaptAndroidTest(Libs.HILT_COMPILER)
+
+    // Glide
+    implementation(Libs.GLIDE)
+    kapt(Libs.GLIDE_COMPILER)
+
+    // Kotlin
+    implementation(Libs.KOTLIN_STDLIB)
+
+    // Instrumentation tests
+    androidTestImplementation(Libs.ESPRESSO_CORE)
+    androidTestImplementation(Libs.ESPRESSO_CONTRIB)
+    androidTestImplementation(Libs.EXT_JUNIT)
+    androidTestImplementation(Libs.RUNNER)
+    androidTestImplementation(Libs.RULES)
+    androidTestImplementation(Libs.FRAGMENT_TEST)
+    debugImplementation(Libs.FRAGMENT_TEST)
+
+    // Local unit tests
+    testImplementation(Libs.JUNIT)
+    testImplementation(Libs.MOCKITO_CORE)
+    testImplementation(Libs.MOCKITO_KOTLIN)
+    testImplementation(Libs.HAMCREST)
+
+    implementation(Libs.GSON)
+}
+apply(plugin = "com.google.gms.google-services")
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,15 @@
+object Versions {
+    val versionName = "1.0.0"
+    private val versionCodeBase = 1
+    val versionCodeMobile = versionCodeBase + 1
+
+    const val COMPILE_SDK = 30
+    const val TARGET_SDK = 30
+    const val MIN_SDK = 21
+
+    const val ANDROID_GRADLE_PLUGIN = "7.0.0-beta05"
+    const val KOTLIN = "1.4.32"
+    const val NAVIGATION = "2.3.5"
+    const val HILT_AGP = "2.36"
+    const val GOOGLE_SERVICES = "4.3.3"
+}
